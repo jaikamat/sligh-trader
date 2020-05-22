46e6d8126ac9dd8bb7a4088c95550198ff5620d5
Admin <jaipkamat@gmail.com>
2020-05-22 12:05:09 -0700

    Pulled in and refactored redundant db upsertion code

--- scrape/fetch_purchase_urls/persistPurchaseLinks.js
+++ scrape/fetch_purchase_urls/persistPurchaseLinks.js
@@ -1,9 +1,4 @@
-require('dotenv').config({ path: '../.env' });
-const MongoClient = require('mongodb').MongoClient;
-const { MONGO_URI } = process.env;
-const { DB_NAME } = process.env;
-const { COLLECTION } = process.env;
-const BATCH_SIZE = 1000;
+const bulkUpsert = require('../common/bulkUpsert');
 
 /**
  * Creates a bulk update operation object
@@ -25,30 +20,10 @@ function createBulkOp({ id, tcg_uri }) {
  * @param {Array} data - List of objects with id and tcg_uri pairs
  */
 async function persistPurchaseLinks(data) {
-    const mongoSettings = { useNewUrlParser: true, useUnifiedTopology: true };
-
     try {
-        var client = await MongoClient.connect(MONGO_URI, mongoSettings);
-        console.log('Connected to Mongo');
-        console.time('Purchase URL bulk write time');
-
-        const bulkOps = data.map(createBulkOp);
-
-        const total = bulkOps.length;
-
-        while (bulkOps.length > 0) {
-            const spliced = bulkOps.splice(0, BATCH_SIZE);
-            await client.db(DB_NAME).collection(COLLECTION).bulkWrite(spliced);
-            const progress = ((1 - bulkOps.length / total) * 100).toFixed(1);
-            console.log(`Bulkwrite in progress: ${progress}%`);
-        }
-
-        console.timeEnd('Purchase URL bulk write time');
+        await bulkUpsert(data.map(createBulkOp));
     } catch (e) {
         throw e;
-    } finally {
-        await client.close();
-        console.log('Disconnected from Mongo');
     }
 }
 
