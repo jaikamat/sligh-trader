0c693d61d01dfb565f00f45d4cb9a0e34416b1af
Admin <jaipkamat@gmail.com>
2020-05-22 09:20:43 -0700

    Squelched catched errors in for loop

--- scrape/fetch_purchase_urls/index.js
+++ scrape/fetch_purchase_urls/index.js
@@ -12,20 +12,27 @@ async function init() {
     try {
         console.time('Purchase link scrape');
         const numDocuments = await getCollectionCount(); // Total number in collection
+        let errors = 0; // TODO: Remove this, log errors correctly
 
         for (let i = 0; i < numDocuments; i += BATCH_SIZE) { // Loop over the documents
-            const cards = await getCards(i, BATCH_SIZE); // In small batch sizes
+            try {
+                const cards = await getCards(i, BATCH_SIZE); // In small batch sizes
 
-            const links = await throttleCardRequests({
-                data: cards,
-                bnConfig: { maxConcurrent: 1, minTime: 125 },
-                fn: getTcgUri
-            });
+                const links = await throttleCardRequests({
+                    data: cards,
+                    bnConfig: { maxConcurrent: 1, minTime: 125 },
+                    fn: getTcgUri
+                });
 
-            await persistPurchaseLinks(links);
+                await persistPurchaseLinks(links);
+            } catch (e) {
+                console.log(e); // Do not throw as we don't want execution of the loop to end
+                errors += 1;
+            }
         }
 
         console.log('TCG purchase link scrape complete');
+        console.log(`There were ${errors} errors`);
         console.timeEnd('Purchase link scrape');
     } catch (e) {
         throw e;
